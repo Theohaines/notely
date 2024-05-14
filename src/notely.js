20088d88cc2b177f1e18067ece38c350186081ac
Theohaines <91698052+Theohaines@users.noreply.github.com>
2024-05-14 22:36:34 +0100

    add welcome menu

--- src/notely.js
+++ src/notely.js
@@ -3,10 +3,6 @@ const path = require('path');
 const dotenv = require('dotenv').config();
 
 const createnote = require('./scripts/createnote.js');
-const savenote = require('./scripts/savenote.js');
-const listnotes = require('./scripts/listnotes.js');
-const loadnote = require('./scripts/loadnote.js');
-const loadnoteview = require('./scripts/loadnoteview.js');
 
 const app = express();
 app.use(express.json());
@@ -32,33 +28,3 @@ app.use('/createnote', async function (req, res){
 
     res.json({"message" : message});
 });
-
-app.use('/savenote', async function (req, res){
-    var message = await savenote.saveNote(req.body.noteName, req.body.noteText);
-
-    res.json({"message" : message});
-});
-
-app.use('/listnotes', async function (req, res){
-    var notes = await listnotes.listNotes();
-
-    res.json(notes);
-})
-
-app.use('/loadnote', async function (req, res){
-    var responsejson = await loadnote.loadNote(req.body.noteName);
-    responsejson = JSON.parse(responsejson)
-
-    if (responsejson.message != "ok"){
-        res.json({"message" : responsejson.message});
-        return;
-    }
-
-    res.json({"message" : "ok", "noteContents" : responsejson.noteContents});
-})
-
-app.use('/loadnoteview', async function (req, res){
-    var notes = await loadnoteview.loadNoteView(req.body.name, req.body.tag);
-
-    res.json(notes);
-})
